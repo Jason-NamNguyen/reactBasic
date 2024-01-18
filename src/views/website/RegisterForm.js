c377c3164f17d539e86f434ce3cf7e8520df25f7
JasonNguyen <nghoangnam93@gmail.com>
2024-01-18 09:04:17 +0700

    update

---
+++ src/views/website/RegisterForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class RegisterForm extends React.Component {
+    render() {
+        return (
+            <div className="">
+                This is my first Project with React
+            </div>
+        )
+    }
+}
+export default RegisterForm;
---
+++ src/views/website/UserLogin.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class UserLogin extends React.Component {
+    render() {
+        let name = `Nam`;
+        return (
+            <div>
+                <h3>Hello World from {name} </h3>
+
+            </div>
+        )
+    }
+
+}
+export default UserLogin;
